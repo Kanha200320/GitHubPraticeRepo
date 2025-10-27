99d2cdda6eafd578c8dd01b1639a2a7a4c6a8ce7
satyamnayak1 <satyamnayak0405@gmail.com>
2025-10-28 00:21:38 +0530

    TL doing the changes.

--- DemoProjGit/src/main/java/com/nt/kanha/App.java
+++ DemoProjGit/src/main/java/com/nt/kanha/App.java
@@ -13,7 +13,7 @@ public class App {
     public static void main(String[] args) {
     	
     	App app=new App();
-    	app.sum(100,100);
+    	System.out.println("The sum is1 "+ app.sum(100,100));
         
     }
 }
