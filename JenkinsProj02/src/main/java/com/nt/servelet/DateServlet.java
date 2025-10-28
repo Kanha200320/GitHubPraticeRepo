6bc2e98db7d4fc2b3f364127b29827d49857968d
satyamnayak1 <satyamnayak0405@gmail.com>
2025-10-29 00:19:40 +0530

    New Project is add.

---
+++ JenkinsProj02/src/main/java/com/nt/servelet/DateServlet.java
@@ -0,0 +1,28 @@
+package com.nt.servelet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet("/dateurl")
+public class DateServlet extends HttpServlet {
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		res.setContentType("text/html");
+		PrintWriter pw=res.getWriter();
+		Date d=new Date();
+		
+		pw.println("<h1> the date is "+d+"</h1>");
+		
+		pw.println("<a href='index.jsp'></a>");
+		pw.close();
+    }
+
+
+}
---
+++ JenkinsProj02/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2 style="color: red;text-align: center;">Welcome to jenkins file</h2>
+<a href="dateurl">View Date</a>
+</body>
+</html>
